461afe1d5e0d19a96947492b8ae00f97efc739a0
VAD <vadimceg@gmail.com>
2018-10-10 22:51:03 +0300

    P1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //P1
+        System.out.println("Welcome to the Shopping Cart");
+
+        //P2
+        
+    }
+}
